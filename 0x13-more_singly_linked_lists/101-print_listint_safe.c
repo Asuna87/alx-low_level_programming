0056ea9127c622477bfac5549911bb7b8c93d6ef
Boutaina <chakirboutaina40@gmail.com>
2023-10-24 15:20:53 -0700

    Singly Linked Lists Project

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,35 +1,63 @@
 #include "lists.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
-* print_listint_safe - Prints a listint_t linked list safely.
-* @head: Pointer to the first node in the list
-* Description: This function prints a singly linked list of integers safely.
-* Return: Number of nodes in the list
-*/
+ * find_loop - Finds the node where the loop in a linked list starts.
+ * @head: A pointer to the head of the listint_t list.
+ *
+ * Return: The address of the node where the loop starts, or NULL if there's no loop.
+ */
+listint_t *find_loop(const listint_t *head)
+{
+    const listint_t *slow, *fast;
+
+    if (head == NULL || head->next == NULL)
+        return NULL;
+
+    slow = head->next;
+    fast = head->next->next;
+
+    while (fast && fast->next)
+    {
+        if (slow == fast)
+        {
+            slow = head;
+            while (slow != fast)
+            {
+                slow = slow->next;
+                fast = fast->next;
+            }
+            return (listint_t *)slow;
+        }
+
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return NULL;
+}
+
+/**
+ * print_listint_safe - Prints a listint_t list safely.
+ * @head: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of nodes in the list.
+ */
 size_t print_listint_safe(const listint_t *head)
 {
-const listint_t *slow, *fast;
-size_t count = 0;
+    size_t nodes = 0;
+    listint_t *loop_start = find_loop(head);
 
-slow = fast = head;
+    while (head != loop_start)
+    {
+        printf("[%p] %d\n", (void *)head, head->n);
+        nodes++;
+        head = head->next;
+    }
 
-while (fast != NULL && fast->next != NULL)
-{
-printf("[%p] %d\n", (void *)slow, slow->n);
-count++;
+    if (loop_start)
+        printf("-> [%p] %d\n", (void *)head, head->n);
 
-slow = slow->next;
-fast = fast->next->next;
-
-if (slow == fast)
-{
-printf("-> [%p] %d\n", (void *)slow, slow->n);
-exit(98);
-}
-}
-
-return (count);
+    return nodes;
 }
 
