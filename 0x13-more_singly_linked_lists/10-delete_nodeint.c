79e018095905c13364837104dca43717bd1c7e38
Boutaina <chakirboutaina40@gmail.com>
2023-10-24 12:49:27 -0700

    Singly Linked Lists Project

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -4,7 +4,6 @@
 /**
  * delete_nodeint_at_index - Deletes the node at a given position
  * in a listint_t list.
- * 
  * @head: Pointer to the pointer of the first node in the list
  * @index: Index of the node to be deleted (starting from 0)
  * Return: 1 if deleted successfully, -1 otherwise.
