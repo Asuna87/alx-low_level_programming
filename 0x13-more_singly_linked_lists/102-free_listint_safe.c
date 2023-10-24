e015b6a65207b190b8e5e3082413a2b0ef6bb71d
Boutaina <chakirboutaina40@gmail.com>
2023-10-24 15:58:06 -0700

    MSLLP

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -9,33 +9,33 @@
  */
 size_t free_listint_safe(listint_t **h)
 {
- size_t len = 0;
- int res;
- listint_t *temp;
+size_t len = 0;
+int res;
+listint_t *temp;
 
- if (!h || !*h)
- return (0);
+if (!h || !*h)
+return (0);
 
- while (*h)
- {
- res = *h - (*h)->next;
- if (res > 0)
- {
- temp = (*h)->next;
- free(*h);
- *h = temp;
- len++;
- }
- else
- {
- free(*h);
- *h = NULL;
- len++;
- break;
- }
- }
+while (*h)
+{
+res = *h - (*h)->next;
+if (res > 0)
+{
+temp = (*h)->next;
+free(*h);
+*h = temp;
+len++;
+}
+else
+{
+free(*h);
+*h = NULL;
+len++;
+break;
+}
+}
 
- *h = NULL;
+*h = NULL;
 
- return (len);
+return (len);
 }
