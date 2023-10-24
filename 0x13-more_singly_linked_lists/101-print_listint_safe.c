f8fa7a6c78a2c7716f8a96def8c2ec7b22b2273d
Boutaina <chakirboutaina40@gmail.com>
2023-10-24 14:56:34 -0700

    Singly Linked Lists Project

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,27 +1,35 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * print_listint_safe - Prints a linked list, even if it has a loop.
- * @head: A pointer to the head of the linked list.
- * Return: The number of nodes in the linked list.
- */
+* print_listint_safe - Prints a listint_t linked list safely.
+* @head: Pointer to the first node in the list
+* Description: This function prints a singly linked list of integers safely.
+* Return: Number of nodes in the list
+*/
 size_t print_listint_safe(const listint_t *head)
 {
-unsigned int count = 0;
-listint_t *current = head;
-listint_t *previous = NULL;
+const listint_t *slow, *fast;
+size_t count = 0;
 
-while (current != NULL) {
-if (current == previous) {
-printf("-> [%p] %d\n", (void *)current, current->n);
-return (count);
-}
+slow = fast = head;
 
-previous = current;
-current = current->next;
+while (fast != NULL && fast->next != NULL)
+{
+printf("[%p] %d\n", (void *)slow, slow->n);
 count++;
+
+slow = slow->next;
+fast = fast->next->next;
+
+if (slow == fast)
+{
+printf("-> [%p] %d\n", (void *)slow, slow->n);
+exit(98);
+}
 }
 
-printf("\n");
 return (count);
 }
+
