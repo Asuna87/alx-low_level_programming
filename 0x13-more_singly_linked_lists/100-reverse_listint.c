7e0a3b72b01c3d8a7516efcecf49421517f20228
Boutaina <chakirboutaina40@gmail.com>
2023-10-24 13:51:58 -0700

    Singly Linked Lists Project

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -10,7 +10,7 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-listint_t *prev = NULL, *current = *head, *next = NULL;
+listint_t *prev = NULL, *current = *head, *next;
 
 while (current != NULL)
 {
