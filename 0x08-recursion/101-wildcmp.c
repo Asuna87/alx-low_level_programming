cad23e9ecc5851c15846b415e5be5b75e8ae8b4c
Boutaina <chakirboutaina40@gmail.com>
2023-10-25 07:53:38 -0700

    Alx Recursion Project

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -6,12 +6,12 @@
 * @s2: The second string with wildcard pattern (*).
 * Return: 1 if the strings can be considered identical, otherwise 0.
 * Description: This function uses recursion to compare strings allowing the
-* use of wildcard character (*) that matches
-* any string (including an empty string).
+* use of wildcard character (*) that
+* matches any string (including an empty string).
 */
 int wildcmp(char *s1, char *s2)
 {
-/* Base case: if s1 and s2 both reach end, they match */
+/* Base case: if both strings are empty, they match */
 if (*s1 == '\0' && *s2 == '\0')
 return (1);
 
@@ -21,7 +21,7 @@ return (wildcmp(s1, s2 + 1));
 
 /* If characters match or s2 has '*', move both pointers */
 if (*s1 == *s2 || *s2 == '*')
-return (wildcmp(s1 + 1, s2 + 1) || wildcmp(s1, s2 + 1));
+return (wildcmp(s1 + 1, s2 + 1) || wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 
 return (0);
 /* Characters don't match and s2 is not '*', strings do not match */
