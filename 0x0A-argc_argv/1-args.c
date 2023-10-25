5e02235e7e902d0e87f548962dd32efd4264bfbc
Boutaina <chakirboutaina40@gmail.com>
2023-10-25 09:27:15 -0700

    Alx Argc Argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Entry point, prints the number of arguments passed
+* @argc: Number of command line arguments
+* @argv: Array containing the command line arguments
+* Return: Always 0
+*/
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
+
