5dbfa91002292e5f306ff0131a78b9d01a4a4a52
Boutaina <chakirboutaina40@gmail.com>
2023-10-25 14:59:32 -0700

    More Malloc Free

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,6 +9,7 @@ int *array_range(int min, int max);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *malloc_checked(unsigned int b);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _putchar(char c);
 
 #endif /* MAIN_H */
 
